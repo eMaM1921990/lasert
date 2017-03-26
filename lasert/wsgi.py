bb3f47a65fc8ee27e05753233cc2b066972384bf
ahmed_elemam <emam151987@gmail.com>
2017-03-26 22:04:39 +0200

    graplli

--- lasert/wsgi.py
+++ lasert/wsgi.py
@@ -19,9 +19,9 @@ import os
 import sys
 
 sys.path.append('/opt/bitnami/apps/django/django_projects/lasert')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/lasert/egg_cache")
+# os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/lasert/egg_cache")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings.local")
 
 from django.core.wsgi import get_wsgi_application
 
