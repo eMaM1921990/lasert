0debd2237c0f3298aaab9e86770a315cbf60a4e7
ahmed_elemam <emam151987@gmail.com>
2017-03-26 21:36:37 +0200

    graplli

--- lasert/wsgi.py
+++ lasert/wsgi.py
@@ -6,11 +6,23 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
+#
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings")
+#
+# application = get_wsgi_application()
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+sys.path.append('/opt/bitnami/apps/django/django_projects/lasert')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/lasert/egg_cache")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
