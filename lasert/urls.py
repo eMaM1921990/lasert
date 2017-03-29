e3ab45b4ab436eeeff3cf5c5991265998d3f438e
ahmed_elemam <emam151987@gmail.com>
2017-03-30 00:21:52 +0200

    cms

--- lasert/urls.py
+++ lasert/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     url(r'^subscribe/$', views.addToSubscriber, name='subscribe'),
     url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
     url(r'^admin/', admin.site.urls),
+    url(r'^content_edit/', include('content_edit.urls')),
 
     # serve media
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
