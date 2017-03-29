5efdbe0b5f5ba9f855da6428b1a91ca56ea1f6bc
ahmed_elemam <emam151987@gmail.com>
2017-03-30 00:26:07 +0200

    django.contrib.sites

--- lasert/settings/base.py
+++ lasert/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'lasertApp',
     'captcha',
     'content_edit',
