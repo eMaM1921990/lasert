a46a06ef9bcff4949f6388055fb199085e5496a4
ahmed_elemam <emam151987@gmail.com>
2017-12-02 18:10:57 +0200

    ini

--- lasert/wsgi.py
+++ lasert/wsgi.py
@@ -7,21 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 #
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings")
-#
-# application = get_wsgi_application()
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings.base")
+
+application = get_wsgi_application()
 
 import os
 import sys
 
-sys.path.append('/opt/bitnami/apps/django/django_projects/lasert')
-os.environ['PYTHON_EGG_CACHE']="/opt/bitnami/apps/django/django_projects/lasert/egg_cache"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lasert.settings.local")
-
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
